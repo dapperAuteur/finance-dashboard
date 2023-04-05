7211d81e7c088103516baa8255a8db2ca4657ccb
awe <a@awews.com>
2023-04-04 20:43:26 -0700

    add filter on affix query

--- src/pages/api/graphql/resolvers/queries/affixQueries.js
+++ src/pages/api/graphql/resolvers/queries/affixQueries.js
@@ -1,20 +1,41 @@
 import clientPromise from "./../../../../../lib/mongodb";
 import shuffle from 'shuffle-array';
 
+const dbCollection = "affixes";
+
 export const AffixQueries = {
     findAffixes: async (_, args, context) => {
-        let {cursor, filter, limit} = {...args}
+        let {cursor, filter, limit} = {...args};
         
         let val = {}
-        let count;
+        let affixes, count;
         try {
             const client = await clientPromise;
             const db = client.db("palabras-express-api");
-            const affixes = await db
+            if (filter) {
+                // count = await db.collection(dbCollection)
+                affixes = await db
+                    .collection(dbCollection)
+                    .find({
+                        $or: [
+                            {
+                                example: {$regex: `${filter}`, $options: "i"}
+                            },{
+                                meaning: {$regex: `{filter}`, $options: "i"}
+                            },{
+                                morpheme: {$regex: `{filter}`, $options: "i"}
+                            }
+                        ]
+                    })
+                    .limit(limit + 1)
+                    .toArray();
+            } else {
+                affixes = await db
                 .collection("affixes")
                 .find({})
                 .limit(limit)
                 .toArray();
+            }
             val = {
                 affixes, count, cursor
             }

0e3369abd875c22fc6e0a2b97d2df7a8929ff13f
awe <a@awews.com>
2023-05-01 11:19:03 -0700

    add 'affix' type to fix ERROR, add vercel analytics

--- src/pages/words/randomAffix.tsx
+++ src/pages/words/randomAffix.tsx
@@ -13,10 +13,12 @@ const fetcher = (query: string) =>
 
 type Data = {
   findRandomAffixes: {
+    affixes: {
     _id: string
     morpheme: string
     example: [string]
     meaning: [string]
+    }
   }[]
 }
 
